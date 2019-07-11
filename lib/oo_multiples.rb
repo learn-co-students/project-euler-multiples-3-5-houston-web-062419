b14ce9dab0fa2654ac992655ea85192a2a9437d7
github username <github email address>
2019-07-11 14:05:02 -0500

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,22 @@
 # Enter your object-oriented solution here!
+class Multiples
+    attr_accessor :limit
+    def initialize(limit)
+        @limit = limit
+    end
+
+
+    def collect_multiples
+        (1...@limit).select do |a|
+            (a % 3 == 0) || (a % 5 == 0)
+        end
+    end
+    
+    
+    def sum_multiples
+        collect_multiples.sum
+    end
+
+
+
+end
