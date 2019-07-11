417ec0b7c3638d415136cb6666029adc88246269
jordanbacon <jmbacon92@gmail.com>
2019-07-11 11:52:07 -0700

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,27 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+    attr_accessor :limit
+
+    @@all = []
+
+    def initialize(limit)
+        @limit = limit
+        @@all << self
+    end
+
+    def self
+        @@all
+    end
+
+    def collect_multiples
+        (1..@limit-1).select do |i|
+            i % 3 == 0 || i % 5 == 0
+        end
+    end
+
+    def sum_multiples
+        (3...@limit).find_all {|n| n % 3 == 0 || n % 5 == 0}.inject(:+)
+    end
+
+end
